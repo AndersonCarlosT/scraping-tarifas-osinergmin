40a978b39fa4d40024643dda20abd47bf9eb4e82
AndersonCarlosT <andercarlost@hotmail.com>
2025-06-02 01:54:29 -0500

    chrome_setup.sh

--- chrome_setup.sh
+++ chrome_setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Actualizar paquetes e instalar wget y unzip por si no están
-apt-get update
-apt-get install -y wget unzip
-
-# Descargar e instalar Google Chrome estable
+# Instalar Google Chrome estable (última versión estable)
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Descargar y descomprimir ChromeDriver versión 114.0.5735.90 (compatible con Chrome estable)
-wget -q https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-mv chromedriver /usr/bin/chromedriver
+# Mostrar versión instalada
+CHROME_VERSION=$(google-chrome --version)
+echo "Versión de Chrome instalada: $CHROME_VERSION"
+
+# Descargar y configurar ChromeDriver versión 137.0.7151.55 (compatible con Chrome 137)
+CHROMEDRIVER_VERSION="137.0.7151.55"
+
+wget -q https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
+mv chromedriver-linux64/chromedriver /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
 
 echo "Chrome y ChromeDriver instalados correctamente"
